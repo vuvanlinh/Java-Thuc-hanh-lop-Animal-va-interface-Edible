940b935417754ae4aaed73e1a48734c6438b795e
vuvanlinh <vuvanlinh95@gmail.com>
2018-10-10 09:25:11 +0700

    thuc hanh lop Animal va interface Edible

---
+++ src/edible/Orange.java
@@ -0,0 +1,10 @@
+package edible;
+
+import fruit.Fruit;
+
+public class Orange extends Fruit {
+    @Override
+    public String howtoEat() {
+        return "Orange could be juiced!";
+    }
+}
---
+++ src/fruit/Apple.java
@@ -0,0 +1,8 @@
+package fruit;
+
+public class Apple extends Fruit {
+    @Override
+    public String howtoEat() {
+        return "apple could be slided";
+    }
+}
